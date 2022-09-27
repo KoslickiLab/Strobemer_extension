41195c2ef82d048a464ee0539ebcf91a10606ddc
Shaopeng Liu <779611291@qq.com>
2022-09-27 12:51:10 -0400

    init

---
+++ src/strobemer_obj_w_extension_and_eval.py
@@ -0,0 +1,14 @@
+### import modules
+from argparse import ArgumentTypeError
+import argparse
+import os
+from Bio import SeqIO
+import re
+import numpy as np
+import random
+import khmer
+import warnings
+import pandas as pd
+import copy
+
+
